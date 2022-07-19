8cd82cce0b079b4c6bba4182efd63f827570e8a5
Oggup <edifoo@gmail.com>
2022-07-19 14:57:54 -0700

    started some dom manipulation for error message

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-let firstName = document.getElementById('firstname');
+let firstName = document.querySelector("#firstname");
+let lastName = document.querySelector("#lastname");
+let email = document.querySelector("#email");
+let phone = document.querySelector("#phone");
+let password = document.querySelector("#password");
+let passwordConfirm = document.querySelector("#password-confirm");
+
+let errorMessage = document.createElement("p");
+errorMessage.classList.add("error");
+errorMessage.textContent = "*passwords do not match";
+
+console.log(errorMessage);
+
+let inputs = [firstName, lastName, email, phone, password, passwordConfirm];
+
+inputs.forEach((input) => {
+  console.log(input);
+});
+
+const submit = (e) => {
+  e.preventDefault();
+  console.log("submit");
+  if (password !== passwordConfirm) {
+    password.classList.add("error");
+    passwordConfirm.classList.add("error");
+    document.querySelector(".confirm-input").append(errorMessage);
+  }
+};
+
+let form = document.querySelector("form");
+form.addEventListener("submit", submit);
