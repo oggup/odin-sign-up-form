e0662a11bd38e412600c0c9f803692f14c770538
Oggup <edifoo@gmail.com>
2022-07-20 12:44:22 -0700

    password check on character length

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let passwordConfirm = document.querySelector("#password-confirm");
 let errorMessage = document.querySelector(".error");
 let passwordConfirmLabel = document.querySelector(".password-confirm");
 let phoneLabel = document.querySelector(".phone");
-
-console.log(errorMessage);
+let passwordLabel = document.querySelector(".password-label");
 
 phone.addEventListener("keyup", () => {
   phoneLabel.classList.add("error");
@@ -22,8 +21,16 @@ phone.addEventListener("keyup", () => {
     phoneLabel.textContent = "phone";
   }
 });
+password.addEventListener("keyup", (e) => {
+  if (e.target.value.length >= 8) {
+    passwordLabel.classList.remove("error");
+    passwordLabel.textContent = "password";
+    console.log(e.target.value.length);
+  }
+});
 
 passwordConfirm.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
   if (password.value !== e.target.value) {
     passwordConfirmLabel.classList.add("error");
   }
@@ -44,6 +51,14 @@ const submit = (e) => {
     passwordConfirmLabel.classList.remove("error");
     passwordConfirmLabel.textContent = "confirm password";
   }
+  if (password.value.length < 8) {
+    passwordLabel.classList.add("error");
+    passwordLabel.textContent = "password must be at least 8 characters long";
+  }
+  if (password.value.length > 8) {
+    passwordLabel.classList.remove("error");
+    passwordLabel.textContent = "password";
+  }
   if (
     !phone.value.match(
       /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
@@ -54,7 +69,6 @@ const submit = (e) => {
     console.log(phone.textContent);
     return;
   }
-  console.log(inputs);
 };
 
 let form = document.querySelector("form");
