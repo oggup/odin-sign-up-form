77aa3331ef56b77457a8502d7a087ac91e3873aa
Oggup <edifoo@gmail.com>
2022-07-19 22:17:06 -0700

    css and dom manipulation when passwords do not match and when the phone is not 10 numbers long

--- script.js
+++ script.js
@@ -4,26 +4,57 @@ let email = document.querySelector("#email");
 let phone = document.querySelector("#phone");
 let password = document.querySelector("#password");
 let passwordConfirm = document.querySelector("#password-confirm");
-
-let errorMessage = document.createElement("p");
-errorMessage.classList.add("error");
-errorMessage.textContent = "*passwords do not match";
+let errorMessage = document.querySelector(".error");
+let passwordConfirmLabel = document.querySelector(".password-confirm");
+let phoneLabel = document.querySelector(".phone");
 
 console.log(errorMessage);
 
 let inputs = [firstName, lastName, email, phone, password, passwordConfirm];
 
-inputs.forEach((input) => {
-  console.log(input);
+phone.addEventListener("keyup", () => {
+  phoneLabel.classList.add("error");
+  phoneLabel.textContent = "phone must be exactly 10 numbers";
+  if (
+    phone.value.match(
+      /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+    )
+  ) {
+    phoneLabel.classList.remove("error");
+    phoneLabel.textContent = "phone";
+  }
 });
 
+passwordConfirm.addEventListener("keyup", (e) => {
+  if (password.value !== e.target.value) {
+    passwordConfirmLabel.classList.add("error");
+    passwordConfirmLabel.textContent = "confirm password";
+  }
+  if (password.value == e.target.value) {
+    passwordConfirmLabel.classList.remove("error");
+  }
+});
 const submit = (e) => {
   e.preventDefault();
   console.log("submit");
-  if (password !== passwordConfirm) {
-    password.classList.add("error");
-    passwordConfirm.classList.add("error");
-    document.querySelector(".confirm-input").append(errorMessage);
+  if (password.value !== passwordConfirm.value) {
+    passwordConfirmLabel.classList.add("error");
+    passwordConfirmLabel.textContent = "*passwords do not match";
+    return;
+  }
+  if (password.value !== passwordConfirm.value) {
+    passwordConfirmLabel.classList.remove("error");
+    passwordConfirmLabel.textContent = "confirm password";
+  }
+  if (
+    !phone.value.match(
+      /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+    )
+  ) {
+    phoneLabel.classList.add("error");
+    phoneLabel.textContent = "phone must be exactly 10 numbers";
+    console.log(phone.textContent);
+    return;
   }
   console.log(inputs);
 };
